d69bf9e980fed12abdcc8fc586f0722b82ad12c2
7ejasVijay <7ejas.vijay@gmail.com>
2024-11-23 17:03:15 +0530

    add sum_digits file

---
+++ sum_digits.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+// Prototype
+int sum_digits(int number);
+
+int main(void) {
+  int number;
+
+  printf("Enter number: ");
+  scanf("%d", &number);
+
+  printf("%d\n", sum_digits(number));
+}
+
+// Uses recursion
+int sum_digits(int number) {
+  if (number < 10) {
+    return number;
+  } else {
+    return (number % 10) + sum_digits(number / 10);
+  }
+}
