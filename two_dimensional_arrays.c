9e0f798bcfb53d83cd9abdc34379446b52afc00e
7ejasVijay <7ejas.vijay@gmail.com>
2024-12-07 15:35:35 +0530

    modify file

--- two_dimensional_arrays.c
+++ two_dimensional_arrays.c
@@ -17,12 +17,22 @@ int main(void) {
   init_matrix(bigger, 100);
 
   // Dynamically allocated arrays
+
   // Method 1 - Memory efficient allocation
+  // ----------------------------------------
   int *two_d_array;
   two_d_array = malloc(sizeof(int) * (3 * 4));
 
   if (two_d_array == NULL) {
     exit(1);
   }
+
+  // The compiler doesn't know the difference between 1D and 2D array, so (i*M + j)
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < M; j++) {
+      two_d_array[i*M + j)] = 0;
+    }
+  }
+  // ----------------------------------------
 }
 
