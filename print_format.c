1033284c4a9ded522cd82680f3afcae2871c384d
7ejasVijay <7ejas.vijay@gmail.com>
2024-12-31 16:06:37 +0530

    modify file

--- print_format.c
+++ print_format.c
@@ -14,6 +14,12 @@ int main(void) {
   printf("%6.1f %6.1f %c\n", x+1, y+1, ch+1);
   printf("%6.1f %6.1f %c\n", x+20, y+20, ch+2);
 
+  int z = 26;
+
+  printf("z is %d in decimal, %x in hexadecimal, and %o in octal\n", z, z, z);
+  printf("ch value is %d which is ASCII value of %c\n", ch, ch);
+  printf("x in scientific notation is %.2e \n", x);
+
   return 0;
 
 }
