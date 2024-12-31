446fe6f7f21d98db5e61b2926a767f38d0b272de
7ejasVijay <7ejas.vijay@gmail.com>
2024-12-31 16:18:33 +0530

    new file

---
+++ scanf_ex.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+  
+  int x;
+  float pi;
+
+  scanf("%d%g", &x, &pi);
+  printf("x is %d and pi is %g\n", x, pi);
+
+  char ch = getchar();
+  putchar(ch);
+
+}
