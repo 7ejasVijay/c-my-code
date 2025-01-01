9a2ddf9f1f63bb4010d3399a74636df818b86726
7ejasVijay <7ejas.vijay@gmail.com>
2025-01-01 17:45:14 +0530

    new file

---
+++ constants.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define N (20)
+#define PI (3.14)
+#define NAME ("John")
+
+int main(void) {
+
+  int array[N];
+  int *d_arr, i;
+  double area, circ, radius;
+
+  radius = 12.3;
+  area = PI*radius*radius;
+  circ = 2*PI*radius;
+
+  d_arr = malloc(sizeof(int) * N);
+  if (d_arr == NULL) {
+    printf("Error: Malloc failed!");
+    exit(1);
+  }
+
+  for(i=0; i<N; i++) {
+    array[i] = i;
+    d_arr[i] = i*2;
+  }
+
+  // ..
+
+}
