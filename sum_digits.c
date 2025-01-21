99d555c6defb2185e963f1c167d66ee6a3797f23
7ejasVijay <7ejas.vijay@gmail.com>
2025-01-21 23:16:43 +0530

    modify file

--- sum_digits.c
+++ sum_digits.c
@@ -12,7 +12,7 @@ int main(void) {
   printf("%d\n", sum_digits(number));
 }
 
-// Uses recursion
+// Uses recursion for summation
 int sum_digits(int number) {
   if (number < 10) {
     return number;
